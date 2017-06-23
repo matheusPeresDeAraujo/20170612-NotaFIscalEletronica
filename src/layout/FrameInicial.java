2b35f6099b2b89c9b0fd143e261c52b2ac932204
matheusPeresDeAraujo <Mathewskta>
2017-06-23 09:38:20 -0300

    indicadores

--- src/layout/FrameInicial.java
+++ src/layout/FrameInicial.java
@@ -176,91 +176,121 @@ public class FrameInicial extends JFrame{
 	
 	private void actionAcessIndicador(){
 		
-//		Total de Notas Fiscais cadastradas
-		EntityManager em = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
-		TypedQuery<Long> query = em.createQuery("select count(n) from Nf n", Long.class);
-		long result = query.getSingleResult();
-		System.out.println(result);
-		
-		em.close();	
-		
-//		Média de valor das notas
-		EntityManager em2 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
-		TypedQuery<Double> query2 = em2.createQuery("select AVG(n.valorItens) from Nf n", Double.class);
-		Double result2 = query2.getSingleResult();
-		System.out.println(result2);
-		
-		em2.close();
-		
-//		Média de valor dos itens das notas
-		EntityManager em3 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
-		TypedQuery<Double> query3 = em3.createQuery("select SUM(n.valorItens)/SUM(n.quantItens) from Nf n", Double.class);
-		Double result3 = query3.getSingleResult();
-		System.out.println(result3);
-		
-		em3.close();
-		
-//		Maior valor de nota
-		EntityManager em4 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
-		TypedQuery<Double> query4 = em4.createQuery("select MAX(n.valorItens) from Nf n", Double.class);
-		Double result4 = query4.getSingleResult();
-		System.out.println(result4);
-		
-		em4.close();
-		
-//		Estado com maior número de notas emitidas
-		EntityManager em5 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
-		TypedQuery<Object[]> query5 = em5.createQuery("select n.emitente.estado, count(*) from Nf n GROUP by n.emitente.estado", Object[].class);
-		List<Object[]> result5 = query5.getResultList();
-		TypedQuery<Object[]> query6 = em5.createQuery("select n.destinatario.estado, count(*) from Nf n GROUP by n.destinatario.estado", Object[].class);
-		List<Object[]> result6 = query6.getResultList();
-		
-		TreeMap<String, Long> resultado = new TreeMap();
-		
-		for(Object[] itens : result5){
-			String name = (String) itens[0];
-			long quant = (Long) itens[1];
+		acessIndicador = new ActionListener() {
 			
-			resultado.put(name, quant);
-		}
-		for(Object[] itens : result6){
-			String name = (String) itens[0];
-			long quant = (Long) itens[1];
-			
-			if(resultado.containsKey(name)){
-				long i =resultado.get(name)+quant;
-				resultado.put(name, i);
-			}else{
-				resultado.put(name, quant);
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+//				Total de Notas Fiscais cadastradas
+				EntityManager em = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
+				TypedQuery<Long> query = em.createQuery("select count(n) from Nf n", Long.class);
+				long result = query.getSingleResult();
+				System.out.println(result);
+				
+				em.close();	
+				
+//				Média de valor das notas
+				EntityManager em2 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
+				TypedQuery<Double> query2 = em2.createQuery("select AVG(n.valorItens) from Nf n", Double.class);
+				Double result2 = query2.getSingleResult();
+				System.out.println(result2);
+				
+				em2.close();
+				
+//				Média de valor dos itens das notas
+				EntityManager em3 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
+				TypedQuery<Double> query3 = em3.createQuery("select SUM(n.valorItens)/SUM(n.quantItens) from Nf n", Double.class);
+				Double result3 = query3.getSingleResult();
+				System.out.println(result3);
+				
+				em3.close();
+				
+//				Maior valor de nota
+				EntityManager em4 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
+				TypedQuery<Double> query4 = em4.createQuery("select MAX(n.valorItens) from Nf n", Double.class);
+				Double result4 = query4.getSingleResult();
+				System.out.println(result4);
+				
+				em4.close();
+				
+//				Estado com maior número de notas emitidas
+				EntityManager em5 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
+				TypedQuery<Object[]> query5 = em5.createQuery("select n.emitente.estado, count(*) from Nf n GROUP by n.emitente.estado", Object[].class);
+				List<Object[]> result5 = query5.getResultList();
+				TypedQuery<Object[]> query6 = em5.createQuery("select n.destinatario.estado, count(*) from Nf n GROUP by n.destinatario.estado", Object[].class);
+				List<Object[]> result6 = query6.getResultList();
+				
+				TreeMap<String, Long> resultado = new TreeMap();
+				
+				for(Object[] itens : result5){
+					String name = (String) itens[0];
+					long quant = (Long) itens[1];
+					
+					resultado.put(name, quant);
+				}
+				for(Object[] itens : result6){
+					String name = (String) itens[0];
+					long quant = (Long) itens[1];
+					
+					if(resultado.containsKey(name)){
+						long i =resultado.get(name)+quant;
+						resultado.put(name, i);
+					}else{
+						resultado.put(name, quant);
+					}
+				}
+				
+				System.out.println(resultado.get(resultado.firstKey()));
+				
+				em5.close();
+				
+//				Estado com maior número de notas como destinatário
+				EntityManager em7 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
+				TypedQuery<Object[]> query7 = em7.createQuery("select n.destinatario.estado, count(*) from Nf n GROUP by n.destinatario.estado", Object[].class);
+				List<Object[]> result7 = query7.getResultList();
+				
+				System.out.println(result7);
+				
+				em7.close();
+				
+//				CNPJ/Nome da empresa que é a maior compradora em volume de vendas
+				EntityManager em8 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
+				TypedQuery<Object[]> query8 = em8.createQuery("select n.destinatario.razaoSocial, count(*) from Nf n GROUP by n.destinatario.razaoSocial", Object[].class);
+				List<Object[]> result8 = query8.getResultList();
+				
+				System.out.println(result8);
+				
+				em8.close();
+				
+//				CNPJ/Nome da empresa que é a maior vendedora em volume de vendas
+				EntityManager em9 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
+				TypedQuery<Object[]> query9 = em9.createQuery("select n.emitente.razaoSocial, count(*) from Nf n GROUP by n.emitente.razaoSocial", Object[].class);
+				List<Object[]> result9 = query9.getResultList();
+				
+				System.out.println(result9);
+				
+				em9.close();
+				
+//				Total de notas com valor superior a 10mil
+				EntityManager em10 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
+				TypedQuery<Object[]> query10 = em10.createQuery("select count(n.valorItens) from Nf n where n.valorItens > 10000", Object[].class);
+				List<Object[]> result10 = query10.getResultList();
+				
+				System.out.println(result10);
+				
+				em10.close();
+				
+//				Total de notas com mais de 10 itens
+				EntityManager em11 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
+				TypedQuery<Object[]> query11 = em11.createQuery("select count(n.quantItens) from Nf n where n.quantItens > 10", Object[].class);
+				List<Object[]> result11 = query11.getResultList();
+				
+				System.out.println(result11);
+				
+				em11.close();
+				
 			}
-		}
-		
-		System.out.println(resultado.get(resultado.firstKey()));
-		
-		em5.close();
-		
-//		Estado com maior número de notas como destinatário
-		EntityManager em7 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
-		TypedQuery<Object[]> query7 = em7.createQuery("select n.destinatario.estado, count(*) from Nf n GROUP by n.destinatario.estado", Object[].class);
-		List<Object[]> result7 = query7.getResultList();
-		
-		System.out.println(result7);
-		
-		em7.close();
-		
-//		CNPJ/Nome da empresa que é a maior compradora em volume de vendas
-		EntityManager em8 = Persistence.createEntityManagerFactory("notaFiscal_unit").createEntityManager();	
-		TypedQuery<Object[]> query8 = em8.createQuery("select n.destinatario.razaoSocial, count(*) from Nf n GROUP by n.destinatario.razaoSocial", Object[].class);
-		List<Object[]> result8 = query8.getResultList();
-		
-		System.out.println(result8);
-		
-		em8.close();
-		
-//		CNPJ/Nome da empresa que é a maior vendedora em volume de vendas
-//		Total de notas com valor superior a 10mil
-//		Total de notas com mais de 10 itens
-		
+		};
 	}
 	
 	private void actionAcessCadastro(){
